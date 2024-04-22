7a18b9d537e91ac1c8ba66948a10c0c72848d693
luizblank <luizrosacardoso@gmail.com>
2024-04-22 09:05:35 -0300

    KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import Login from './pages/Login.js';
+import Form from './pages/Form.js';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login/>
+  },
+  {
+    path: '/formulario',
+    element: <Form/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
