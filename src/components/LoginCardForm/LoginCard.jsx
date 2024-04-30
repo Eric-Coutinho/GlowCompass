7ca0b741509cac5361971c7d8c7e13fcd971046b
luizblank <luizrosacardoso@gmail.com>
2024-04-30 09:03:49 -0300

    Added navigation

--- src/components/LoginCardForm/LoginCard.jsx
+++ src/components/LoginCardForm/LoginCard.jsx
@@ -13,26 +13,28 @@ export default function LoginCard() {
             <Card className={styles.card}>
                 <Card.Body className={styles.cardBody}>
                     <Card.Title className={styles.title} as="h2">Login</Card.Title>
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="Email"
-                        className="mb-3"
-                    >
-                        <Form.Control type="email" placeholder="name@example.com"/>
-                    </FloatingLabel>
-                    <FloatingLabel controlId="floatingPassword" label="Senha">
-                        <Form.Control type="password" placeholder="Senha" />
-                    </FloatingLabel>
-                    <div className={styles.recupera}>
-                        <Link style={{ color: '#D4A373' }}>Recuperar Senha</Link>
-                    </div>
-                    <div className={styles.buttons}>
-                        <div className="d-grid gap-2">
-
-                            <Button className={styles.btn} style={{ marginBlock: '0.3em' }}>Entrar</Button>{' '}
-                            <Button className={styles.btn} style={{ marginBottom: '0.5em' }}>Registrar-se</Button>{' '}
+                    <form action="/formulario" style={{ width: '100%' }}>
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="Email"
+                            className="mb-3"
+                            style={{ width: '100%' }}
+                        >
+                            <Form.Control type="email" placeholder="name@example.com" />
+                        </FloatingLabel>
+                        <FloatingLabel controlId="floatingPassword" label="Senha" style={{ width: '100%' }}>
+                            <Form.Control type="password" placeholder="Senha" />
+                        </FloatingLabel>
+                        <div className={styles.recupera}>
+                            <Link style={{ color: '#D4A373' }}>Recuperar Senha</Link>
                         </div>
-                    </div>
+                        <div className={styles.buttons}>
+                            <div className="d-grid gap-2">
+                                <Button className={styles.btn} style={{ marginBlock: '0.3em' }}>Entrar</Button>{' '}
+                                <Button className={styles.btn} style={{ marginBottom: '0.5em' }}>Registrar-se</Button>{' '}
+                            </div>
+                        </div>
+                        </form>
                 </Card.Body>
             </Card>
         </>
